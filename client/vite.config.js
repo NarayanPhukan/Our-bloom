61085edd854562b198d10cb36e9001e674fd476b
Lencho17 <lenchosolutions17@gmail.com>
2026-09-02 02:33:53 +0530

    fix terser build error

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,13 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    target: 'esnext'
+  },
+  esbuild: {
+    drop: ['console', 'debugger']
   }
 })
